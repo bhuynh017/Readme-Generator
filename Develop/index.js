aa866a907e8f0aa5b41ab17699c340907934af10
Bryant Huynh <bryanthuynh@Bryants-MBP.lan>
2022-10-19 11:34:12 -0500

    setting requirements on index.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,39 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const path = require("path");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+
+    {
+        type: "input",
+        name: "github",
+        message: "What's your Github username?"
+    },
+
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email?"
+    },
+    
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.createPromptModule(questions)
+    .then((inquirerAnswers) => {
+        console.log("Generating..... Please wait....");
+        writeToFile("./dist/README.md", generateMarkdown({ ...inquirerAnswers}));
+    })
+}
 
 // Function call to initialize app
 init();
