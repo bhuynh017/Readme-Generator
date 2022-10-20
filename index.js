f13169a0954428b043af16695ec589ab344209dc
Bryant Huynh <bryanthuynh@Bryants-MBP.lan>
2022-10-19 19:30:50 -0500

    update index.js

--- index.js
+++ index.js
@@ -19,6 +19,49 @@ const questions = [
         message: "What is your email?"
     },
     
+    {
+        type: "input",
+        name: "title",
+        message: "What's the name of your project?"
+    },
+    
+    {
+        type: "input",
+        name: "description",
+        message: "Describe about your project:"
+    },
+
+    {
+        type: "list",
+        name: "license",
+        message: "What license does your project have?",
+        choices: ["MIT", "APACHE2.0", "Boost1.0", "GPL3.0", "BSD2" ,"BSD3", "None"]
+    },
+
+    {
+        type: "input",
+        name: "dependencies",
+        message: "Any dependencies to install?",
+        default: "npm i"
+    },
+
+    {
+        type: "input",
+        name: "test",
+        message: "What command should be run to run tests?",
+        default: "npm test"
+    },
+
+    {
+        type: "input",
+        name: "usage",
+        message: "What does the usage of this repo?",
+    },
+    {
+        type: "input",
+        name: "contributors",
+        message: "Who are the contributors of this repo?",
+    }
 ];
 
 // TODO: Create a function to write README file
